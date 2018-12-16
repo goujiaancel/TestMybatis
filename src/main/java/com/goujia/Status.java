ab0b76c910cc4bf0a141a90276b752f76b982fe4
goujia <goujiaancel@126.com>
2018-12-16 22:54:43 +0800

    test first push add Status.java

--- src/main/java/com/goujia/Status.java
+++ src/main/java/com/goujia/Status.java
@@ -1,4 +1,48 @@
 package com.goujia;
 
+import org.apache.commons.lang3.StringUtils;
+
 public enum Status {
+    A_CODE(400,"Gmail"),
+    B_CODE(500,"FoxMail")
+    ;
+    public int code;
+    public String msg;
+
+    Status() {
+    }
+
+    Status(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Status getStatus(Status externalStatus){
+        if(null==externalStatus){
+            return null;
+        }
+        for(Status status:values()){
+            if(StringUtils.equals(externalStatus.getMsg(),status.getMsg())&&externalStatus.code==status.code){
+                return status;
+            }
+        }
+        return null;
+    }
+
 }
