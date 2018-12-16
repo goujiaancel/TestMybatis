b8c70030e034a61ac4b3fcef7dbe3563bdc17d48
goujia <goujiaancel@126.com>
2018-12-16 22:53:20 +0800

    test first push

---
+++ src/main/java/com/goujia/Status.java
@@ -0,0 +1,4 @@
+package com.goujia;
+
+public enum Status {
+}
---
+++ src/main/java/com/goujia/UserInfo.java
@@ -0,0 +1,62 @@
+package com.goujia;
+
+import java.util.Date;
+
+public class UserInfo {
+    private Long id;
+    private String name;
+    private Long age;
+    private Date createdTime;
+    private Date updatedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", createdTime=" + createdTime +
+                ", updatedTime=" + updatedTime +
+                '}';
+    }
+}
